19b73e24a90a4a1893150e1bae674075a7a98b1a
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-04-01 14:25:08 +0100

    creared a consts for excos and projects, update later

---
+++ src/constants/Index.jsx
@@ -0,0 +1,75 @@
+export const excos =[
+    {
+        name: "",
+        images: "",
+        biodata: "",
+    },
+    {
+        name: "",
+        images: "",
+        biodata: "",
+    },
+    {
+        name: "",
+        images: "",
+        biodata: "",
+    },
+    {
+        name: "",
+        images: "",
+        biodata: "",
+    },
+    {
+        name: "",
+        images: "",
+        biodata: "",
+    },
+    {
+        name: "",
+        images: "",
+        biodata: "",
+    },
+    {
+        name: "",
+        images: "",
+        biodata: "",
+    },
+    {
+        name: "",
+        images: "",
+        biodata: "",
+    },
+];
+
+export const projects =[
+    {
+        title: "Project One",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ullam.",
+        image: ".png",
+    },
+    {
+        title: "Project Two",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ullam.",
+        image: ".png",
+    },
+    {
+        title: "Project Three",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ullam.",
+        image: ".png",
+    },
+    {
+        title: "Project Four",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ullam.",
+        image: ".png",
+    },
+    {
+        title: "Project Five",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ullam.",
+        image: ".png",
+    },
+    {
+        title: "Project Six",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ullam.",
+        image: ".png",
+    },
+]
